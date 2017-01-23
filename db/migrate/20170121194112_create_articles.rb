ec4ccfd8fb7c3dbdd1e361d4dbd1248bb8af80e9
OleMartinS <osae005@yahoo.com>
2017-01-23 22:05:29 +0100

    Part6
    Feilmeldingene skal funke. men finner ikke ut hvorfor det kommer en
    ekstra linje hvor alle feilmeldingene er ramset opp.

--- db/migrate/20170121194112_create_articles.rb
+++ db/migrate/20170121194112_create_articles.rb
@@ -1,8 +1,8 @@
 class CreateArticles < ActiveRecord::Migration[5.0]
   def change
     create_table :articles do |t|
-      t.string :tittel
-      t.text :tekst
+      t.string :title
+      t.text :text
 
       t.timestamps
     end
