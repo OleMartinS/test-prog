219c99dfacbe923dc3ae0537581930611c5cb08c
OleMartinS <osae005@yahoo.com>
2017-02-07 19:11:41 +0100

    Destroy comments, login
    Now there should be able to destroy comments, and you can only destroy
    articles and comments if you are logged in.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "ole", password: "passord", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
